8cde81ab0e593bb9aef9aad9630503baaec24911
Robin Claesson <57729281+RobinClaesson@users.noreply.github.com>
2020-12-02 23:57:25 +0100

    2017 Day 2020

---
+++ C#/2017/Day 20/Day 20.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AoC;
+namespace Day_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = IO.InputRows;
+
+            List<Particle> particles = new List<Particle>();
+
+            foreach (string row in input)
+                particles.Add(new Particle(row));
+
+            int index = 0;
+
+            //Part 1
+            for (int i = 0; i < particles.Count; i++)
+            {
+                if (particles[i].a.Length() < particles[index].a.Length())
+                    index = i;
+            }
+
+            IO.Output(index);
+
+
+            //Part 2
+            int lastCount = -1;
+            int updatesSinceCol = 0;
+            while (updatesSinceCol < 1000)
+            {
+                lastCount = particles.Count;
+                //Update
+                foreach (Particle particle in particles)
+                    particle.Update();
+
+                //check collision
+                for (int i = 0; i < particles.Count; i++)
+                    for (int j = 0; j < particles.Count; j++)
+                        if (i != j && particles[i].p == particles[j].p)
+                        {
+                            particles[i].alive = false;
+                            particles[j].alive = false;
+                            updatesSinceCol = 0;
+                        }
+
+                //Removes dead particles
+                for (int i = 0; i < particles.Count; i++)
+                    if (!particles[i].alive)
+                    {
+                        particles.RemoveAt(i);
+                        i--;
+                    }
+
+                updatesSinceCol++;
+            }
+
+            
+            IO.Output(particles.Count);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/2017/Day 20/Particle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Day_20
+{
+    class Particle
+    {
+        public Vector3 p, v, a;
+        public bool alive = true;
+        public Particle(string data)
+        {
+            data = data.Replace("<","");
+            data = data.Replace(">", "");
+            data = data.Replace("=", "");
+            data = data.Replace("p", "");
+            data = data.Replace("a", "");
+            data = data.Replace("v", "");
+            data = data.Replace(" ", "");
+
+            string[] values = data.Split(',');
+
+            p = new Vector3(int.Parse(values[0]), int.Parse(values[1]), int.Parse(values[2]));
+            v = new Vector3(int.Parse(values[3]), int.Parse(values[4]), int.Parse(values[5]));
+            a = new Vector3(int.Parse(values[6]), int.Parse(values[7]), int.Parse(values[8]));
+
+        }
+
+        public void Update()
+        {
+            if (alive)
+            {
+                v.X += a.X;
+                v.Y += a.Y;
+                v.Z += a.Z;
+
+                p.X += v.X;
+                p.Y += v.Y;
+                p.Z += v.Z;
+            }
+        }
+
+        public float ManhattanDist
+        {
+            get
+            {
+                return Math.Abs(p.X) + Math.Abs(p.Y) + Math.Abs(p.Z);
+            }
+        }
+    }
+}
