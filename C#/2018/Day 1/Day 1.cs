4268b9d8d958a9f6b1ee66b4bcbae0f188c9174e
Robin Claesson <57729281+RobinClaesson@users.noreply.github.com>
2021-09-10 22:08:59 +0200

    2018 first day

---
+++ C#/2018/Day 1/Day 1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AoC;
+namespace Day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = IO.InputRows_Int;
+
+            //Part 1
+            IO.Output(input.Sum());
+
+            //Part 2
+            List<int> sums = new List<int>();
+            sums.Add(0);
+            bool match = false;
+
+            while (!match)
+                for (int i = 0; i < input.Count; i++)
+                {
+                    int sum = sums.Last() + input[i];
+
+                    if (sums.IndexOf(sum) != -1)
+                    {
+                        match = true;
+                        IO.Output(sum);
+                        break;
+                    }
+
+                    sums.Add(sum);
+                }
+
+            Console.WriteLine("Done!");
+            Console.ReadKey();
+        }
+    }
+}
