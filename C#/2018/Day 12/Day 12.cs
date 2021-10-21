c1b0282094fabbbeebec975ca6fcbc755a23a545
Robin Claesson <57729281+RobinClaesson@users.noreply.github.com>
2021-10-21 16:27:13 +0200

    2018 day 12 part 2

--- C#/2018/Day 12/Day 12.cs
+++ C#/2018/Day 12/Day 12.cs
@@ -23,11 +23,14 @@ namespace Day_12
                 rules[row.Substring(0, 5)] = row.Last();
             }
 
+            List<string> seenStates = new List<string>();
+            seenStates.Add(CleanState(currentState));
             //Console.WriteLine("0: " + currentState);
 
-            int startIndex = 0;
+            long startIndex = 0;
             //Growth generations
-            for (int generation = 1; generation <= 20; generation++)
+            long generation = 1;
+            while (generation <= 20)
             {
                 string nextState = "";
 
@@ -45,7 +48,6 @@ namespace Day_12
                     nextState += NextGen(currentState.Substring(i - 2, 5), rules);
 
                 //Check last two incase we need to add empty pots to the begining
-                //string sub = currentState.Substring(currentState.Length - 3) + "..";
                 nextState += NextGen(currentState.Substring(currentState.Length - 4) + ".", rules);
                 nextState += NextGen(currentState.Substring(currentState.Length - 3) + "..", rules);
 
@@ -54,18 +56,80 @@ namespace Day_12
                     nextState += "..";
 
                 currentState = nextState;
-                //Console.WriteLine(generation + ": " + currentState);
+                seenStates.Add(CleanState(currentState));
+
+                generation++;
             }
 
             //Sum number on pots
-            int sum = 0;
-            for(int i = 0; i < currentState.Length; i++)
+            long sum = 0;
+            for (int i = 0; i < currentState.Length; i++)
             {
                 if (currentState[i] == '#')
                     sum += startIndex + i;
             }
+            IO.Output(sum);
 
-            
+            //Part 2
+            //By testing i found that after a certain number of repititions the plants just move to the right
+            while (generation <= 50000000000)
+            {
+                string nextState = "";
+
+                //Check first two incase we need to add empty pots to the begining
+                nextState += NextGen(".." + currentState.Substring(0, 3), rules);
+                nextState += NextGen("." + currentState.Substring(0, 4), rules);
+
+                if (nextState.Contains("#"))
+                {
+                    nextState = ".." + nextState;
+                    startIndex -= 2;
+                }
+
+                for (int i = 2; i < currentState.Length - 2; i++)
+                    nextState += NextGen(currentState.Substring(i - 2, 5), rules);
+
+                //Check last two incase we need to add empty pots to the begining
+                nextState += NextGen(currentState.Substring(currentState.Length - 4) + ".", rules);
+                nextState += NextGen(currentState.Substring(currentState.Length - 3) + "..", rules);
+
+                string sub = nextState.Substring(nextState.Length - 2);
+                if (nextState.Substring(nextState.Length - 2).Contains("#"))
+                    nextState += "..";
+
+                currentState = nextState;
+
+                string cleanCurrent = CleanState(currentState);
+
+                if (!seenStates.Contains(cleanCurrent))
+                {
+                    seenStates.Add(CleanState(currentState));
+                }
+
+                //The plants only move one step to the right from here on
+                //So we can just move the start index the remaindings generations and stop
+                else
+                {
+                    //Debug values used to figure out what was going on
+                    //int index = seenStates.IndexOf(cleanCurrent);
+                    //int firstPlant = currentState.IndexOf("#");
+                    //int lastPlant = currentState.LastIndexOf("#");
+                    //int diff = lastPlant - firstPlant;
+
+                    startIndex += 50000000000 - generation;
+                    break;
+                }
+
+                generation++;
+            }
+
+            //Sum number on pots
+            sum = 0;
+            for (int i = 0; i < currentState.Length; i++)
+            {
+                if (currentState[i] == '#')
+                    sum += startIndex + i;
+            }
 
             IO.Output(sum);
             Console.ReadKey();
@@ -78,6 +142,14 @@ namespace Day_12
 
 
             return '.';
+        }
+
+
+        public static string CleanState(string state)
+        {
+            int first = state.IndexOf("#");
+            int last = state.LastIndexOf("#");
+            return state.Substring(first, last - first + 1);
         }
     }
 }
