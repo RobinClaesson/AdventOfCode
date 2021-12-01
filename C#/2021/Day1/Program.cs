9a6267aa0462db7e644f886c4e951e5e0e49c8f3
Robin Claesson <57729281+RobinClaesson@users.noreply.github.com>
2021-12-01 16:01:39 +0100

    2021 Day 1

---
+++ C#/2021/Day1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AoC;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = IO.InputRows_Int;
+
+            int increased = 0;
+            for (int i = 1; i < input.Count; i++)
+                if (input[i] > input[i - 1])
+                    increased++;
+
+            IO.Output(increased);
+
+            increased = 0;
+            for (int i = 1; i < input.Count - 2; i++)
+            {
+                int curr = input[i] + input[i + 1] + input[i + 2];
+                int prev = input[i - 1] + input[i] + input[i + 1];
+
+                if (prev < curr)
+                    increased++;
+            }
+
+            IO.Output(increased);
+            Console.ReadKey();
+
+        }
+    }
+}
