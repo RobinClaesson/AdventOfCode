5f62d0eeced3512b4dd96f7fce5a4eb642219810
Robin Claesson <robin.claesson@gmail.com>
2022-12-04 13:18:24 +0100

    2022 Day 4

---
+++ C#/2022/Day4/Day4.cs
@@ -0,0 +1,22 @@
+﻿using AoC.IO;
+
+Input.TestMode = false;
+var input = Input.Rows;
+
+var part1 = 0;
+var part2 = 0;
+
+foreach(var row in input)
+{
+    var numbers = row.Replace(',', '-').Split('-').Select(s => int.Parse(s)).ToList();
+
+    if ((numbers[0] <= numbers[2] && numbers[1] >= numbers[3]) || (numbers[0] >= numbers[2] && numbers[1] <= numbers[3]))
+        part1++;
+
+    if ((numbers[0] >= numbers[2] && numbers[0] <= numbers[3]) || (numbers[2] >= numbers[0] && numbers[2] <= numbers[1]))
+        part2++;
+
+
+}
+Output.Answer(part1);
+Output.Answer(part2);
