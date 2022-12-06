3c655a58d66eaa3505bb10e377e48ffdda998287
Robin Claesson <robcla@kth.se>
2022-12-06 13:32:48 +0100

    2022 Day 5

---
+++ C#/AoC_DotNet6/Tools/Extractor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC.Tools
+{
+    public class Extractor
+    {
+        /// <summary>
+        /// Gets all the integers in a string with sections separated by a char
+        /// </summary>
+        /// <param name="input">string to extract ints from</param>
+        /// <param name="separator">separator to split the string by</param>
+        /// <returns>Array of all integers in the string</returns>
+        public static int[] GetIntsInString(string input, char separator)
+        {
+            return input.Split(separator).Where(
+                s => int.TryParse(s, out int n)
+                ).Select(s => int.Parse(s)).ToArray();
+        }
+
+    }
+}
