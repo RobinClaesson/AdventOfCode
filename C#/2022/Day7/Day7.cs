cc7c83c1c67b14385d1897617607468f0497eacb
Robin Claesson <robin.claesson@gmail.com>
2022-12-07 13:28:21 +0100

    2022 Day 7

---
+++ C#/2022/Day7/Day7.cs
@@ -0,0 +1,50 @@
+﻿using AoC.IO;
+using Day7;
+
+Input.TestMode = false;
+var input = Input.Rows;
+
+var root = new Dir() { Name = "/" };
+var current = root;
+
+for (int i = 1; i < input.Count; i++)
+{
+    if (input[i] == "$ ls")
+    {
+        while (i + 1 < input.Count && !input[i+1].Contains("$"))
+        {
+            i++;
+            var split = input[i].Split(' ');
+
+            if (split[0] == "dir")
+            {
+                current.SubDirectories.Add(new Dir() { Name = split[1], Parent = current });
+            }
+            else
+            {
+                current.Files.Add(split[1], int.Parse(split[0]));
+            }
+        }
+    }
+
+    if (input[i].Contains("$ cd"))
+    {
+        var split = input[i].Split(' ');
+
+        if (split[2] == "/")
+            current = root;
+        else if (split[2] == "..")
+            current = current.Parent;
+        else
+            current = current.SubDirectories.Where(x => x.Name == split[2]).First();
+    }
+}
+
+Output.Answer(root.Part1());
+
+int free = 70000000 - root.Size();
+int needToDelete = 30000000 - free;
+
+Output.Answer(root.Sizes().Where(i => i > needToDelete).Min());
+
+
---
+++ C#/2022/Day7/Dir.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7
+{
+    public class Dir
+    {
+        public string Name { get; set; }
+        public Dir Parent { get; set; }
+
+        public List<Dir> SubDirectories { get; set; } = new List<Dir>();
+        public Dictionary<string, int> Files { get; set; } = new Dictionary<string, int>();
+
+        public int Size()
+        {
+            var size = Files.Values.Sum();
+            var subSize = SubDirectories.Select(x => x.Size()).Sum();
+            return size + subSize;
+        }
+
+        public int Size(string dir)
+        {
+            if(Name == dir)
+                return Size();
+            else 
+                return SubDirectories.Select(x => x.Size(dir)).Sum();
+        }
+
+        public int Part1()
+        {
+            var sum = 0;
+            var size = Size();
+
+            if (size <= 100000)
+                sum += size;
+
+            sum += SubDirectories.Select(x => x.Part1()).Sum();
+
+            return sum;
+        }
+
+        public List<int> Sizes()
+        {
+            List<int> list = new List<int> { Size() };
+
+            foreach (var sub in SubDirectories)
+                list.AddRange(sub.Sizes());
+
+            return list;
+        }
+    }
+}
