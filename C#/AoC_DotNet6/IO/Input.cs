b8c3dd2b54693173a069ea3b1867504fbace63a1
Robin Claesson <robcla@kth.se>
2023-11-24 17:06:52 +0100

    Divided LINQ calls to rows for readability

--- C#/AoC_DotNet6/IO/Input.cs
+++ C#/AoC_DotNet6/IO/Input.cs
@@ -21,36 +21,48 @@
 
         private static string fileToRead = InputFile;
 
-        public static string All => File.ReadAllText(fileToRead);
+        public static string All 
+            => File.ReadAllText(fileToRead);
 
         public static int AllAsInt
             => int.Parse(All);
 
         public static List<string> Rows 
-            => File.ReadLines(fileToRead).ToList();
+            => File.ReadLines(fileToRead)
+                .ToList();
 
         public static List<int> RowsAsInt
-            => File.ReadLines(fileToRead).Where(r => r != "").Select(r => int.Parse(r)).ToList();
+            => File.ReadLines(fileToRead)
+                .Where(r => r != "")
+                .Select(r => int.Parse(r))
+                .ToList();
 
 
         public static List<List<int>> IntGrid 
-            => File.ReadLines(fileToRead).Select(r => r.Select(c => int.Parse($"{c}")).ToList()).ToList();
+            => File.ReadLines(fileToRead)
+                .Select(r => r.Select(c => int.Parse($"{c}")).ToList())
+                .ToList();
 
         public static List<string> Split(char separator)
-            => All.Split(separator).ToList();
+            => All
+                .Split(separator)
+                .ToList();
 
         public static List<string[]> RowsSplitted(char separator)
-            => File.ReadLines(fileToRead).Select(s => s.Split(separator)).ToList();
+            => File.ReadLines(fileToRead)
+                .Select(s => s.Split(separator))
+                .ToList();
 
         public static List<int[]>RowsSplittedAsInt(char separator)
-            => File.ReadLines(fileToRead).Select(s => s.Split(separator)
-                                        .Where(s => s != string.Empty)
-                                        .Select(s => int.Parse(s)).ToArray())
-                                        .ToList();
+            => File.ReadLines(fileToRead)
+                .Select(s => s.Split(separator)
+                .Where(s => s != string.Empty)
+                .Select(s => int.Parse(s)).ToArray())
+                .ToList();
 
         public static List<int> SplitAsInt(char separator)
-            => All.Split(separator).Select(s => int.Parse(s)).ToList();
-        
-
+            => All.Split(separator)
+                .Select(s => int.Parse(s))
+                .ToList();
     }
 }
