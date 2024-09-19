4352b6b58c0d2c11e3f95d1decfc26c536d97505
Robin Claesson <robcla@kth.se>
2024-09-19 14:02:52 +0200

    C# 2019 Day 1

--- C#/2019/Day1/Day1.cs
+++ C#/2019/Day1/Day1.cs
@@ -6,3 +6,16 @@ var input = Input.RowsAsInt;
 
 var part1 = input.Select(x => x/3 - 2).Sum();
 Output.Answer(part1);
+
+var part2 = input.Select(x =>
+{
+    var total = 0;
+    while (x > 0)
+    {
+        x = x/3 - 2;
+        if (x > 0) 
+            total += x;
+    }
+    return total;
+}).Sum();
+Output.Answer(part2);
