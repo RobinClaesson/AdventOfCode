bd207989f8da3d35283bdfe1ed02f117cded40d8
Robin Claesson <robcla@kth.se>
2024-09-19 14:00:26 +0200

    Replaced 2019 with new projects
    Remakes of all previous days incoming

---
+++ C#/2019/Day1/Day1.cs
@@ -0,0 +1,8 @@
+﻿using AoC.IO;
+
+Input.TestMode = false;
+
+var input = Input.RowsAsInt;
+
+var part1 = input.Select(x => x/3 - 2).Sum();
+Output.Answer(part1);
--- C#/2019/README.md
+++
@@ -1,4 +0,0 @@
-# 2019
-This is not yet imported to use the correct libraries and wont work right now
-
-
