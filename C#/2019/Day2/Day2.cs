d10ae1276445ee48be9af3fe85a4c0d7b17f228a
Robin Claesson <robcla@kth.se>
2024-09-19 14:35:04 +0200

    C# 2019 Day 2
    Including IntCodeComputer base

---
+++ C#/2019/Day2/Day2.cs
@@ -0,0 +1,41 @@
+﻿using AoC.IO;
+using AoC2019Libraries;
+
+Input.TestMode = false;
+
+var input = Input.SplitAsInt(',');
+
+//Part 1
+var computer = new IntCodeComputer(input, 12, 2);
+computer.Run();
+Output.Answer(computer[0]);
+
+var a = 0;
+var b = 0;
+var target = 19690720;
+
+while (true)
+{
+    computer = new IntCodeComputer(input, a, b);
+    computer.Run();
+    if (computer[0] == target)
+    {
+        Output.Answer(100 * a + b);
+        break;
+    }
+
+    computer = new IntCodeComputer(input, b, a);
+    computer.Run();
+    if (computer[0] == target)
+    {
+        Output.Answer(100 * b + a);
+        break;
+    }
+
+    a++;
+    if(a > b)
+    {
+        b++;
+        a = 0;
+    }
+}
---
+++ C#/2019/IntcodeComputer/IntCodeComputer.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+
+namespace AoC2019Libraries
+{
+    public class IntCodeComputer
+    {
+        private List<int> program = new List<int>();
+        private int instructionPointer = 0;
+
+        public IntCodeComputer(IEnumerable<int> program)
+        {
+            this.program = program.ToList();
+        }
+
+        public IntCodeComputer(IEnumerable<int> program, int noun, int verb)
+        {
+            this.program = program.ToList();
+            this.program[1] = noun;
+            this.program[2] = verb;
+        }
+
+        public int this[int index]
+        {
+            get { return program[index]; }
+            set { program[index] = value; }
+        }
+
+        public override string ToString()
+            => string.Join(",", program);
+
+        public void Run()
+        {
+            while (program[instructionPointer] != 99)
+            {
+                switch (program[instructionPointer])
+                {
+                    case 1:
+                        Opcode1();
+                        break;
+                    case 2:
+                        Opcode2();
+                        break;
+                    default:
+                        throw new Exception("Invalid opcode");
+                }
+
+            }
+        }
+
+        private void Opcode1()
+        {
+            var a = program[instructionPointer + 1];
+            var b = program[instructionPointer + 2];
+            var to = program[instructionPointer + 3];
+            program[to] = program[a] + program[b];
+            instructionPointer += 4;
+        }
+
+        private void Opcode2()
+        {
+            var a = program[instructionPointer + 1];
+            var b = program[instructionPointer + 2];
+            var to = program[instructionPointer + 3];
+            program[to] = program[a] * program[b];
+            instructionPointer += 4;
+        }
+    }
+}
