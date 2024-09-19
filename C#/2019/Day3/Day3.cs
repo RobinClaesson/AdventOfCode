8957e70c9f0ccdb62e1f09fec4b75142e456b8f1
Robin Claesson <robcla@kth.se>
2024-09-19 15:07:01 +0200

    C# 2019 Day 3

---
+++ C#/2019/Day3/Day3.cs
@@ -0,0 +1,51 @@
+﻿using AoC.IO;
+using System.Numerics;
+
+var input = Input.RowsSplitted(',');
+
+
+var wire1 = TraceWire(input[0]);
+var wire2 = TraceWire(input[1]);
+
+var part1 = wire1.Intersect(wire2)
+                 .Where(p => p != Vector2.Zero)
+                 .Select(p => Math.Abs(p.X) + Math.Abs(p.Y))
+                 .Min();
+
+Output.Answer(part1) ;
+
+var part2 = wire1.Intersect(wire2)
+                 .Where(p => p != Vector2.Zero)
+                 .Select(p => wire1.IndexOf(p) + wire2.IndexOf(p))
+                 .Min();
+
+Output.Answer(part2);
+
+List<Vector2> TraceWire(string[] wireInfo)
+{
+    var wirePoints = new List<Vector2> { Vector2.Zero };
+    
+    foreach(var instruction in wireInfo)
+    {
+        var direction = instruction[0];
+        var distance = int.Parse(instruction[1..]);
+        
+        var lastPoint = wirePoints.Last();
+        
+        for (int i = 1; i <= distance; i++)
+        {
+            var newPoint = direction switch
+            {
+                'U' => lastPoint + new Vector2(0, -i),
+                'D' => lastPoint + new Vector2(0, i),
+                'L' => lastPoint + new Vector2(-i, 0),
+                'R' => lastPoint + new Vector2(i, 0),
+                _ => throw new Exception("Invalid direction")
+            };
+            
+            wirePoints.Add(newPoint);
+        }
+    }
+
+    return wirePoints;
+}
