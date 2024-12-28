0d171e4c758dfcd87094cd19146061dc05f8abdb
Robin Claesson <robcla@kth.se>
2024-12-28 17:08:44 +0100

    C# 2024 Day 12 Part 2

--- C#/2024/Day12/Day12.cs
+++ C#/2024/Day12/Day12.cs
@@ -50,8 +50,91 @@ for (int y = 0; y < input.Count; y++)
 }
 Console.WriteLine("Regions found");
 
-var part1 = regions.Sum(r => r.Count * GetPerimeter(r));
+var part1 = regions.Sum(r => r.Count * GetPerimeterLength(r));
 Output.Answer(part1);
-int GetPerimeter(IEnumerable<Vector2> region)
+
+// The number of sides are the same as the number of corners
+var part2 = regions.Sum(r => r.Count * GetCornerCount(r));
+Output.Answer(part2);
+
+
+
+int GetPerimeterLength(IEnumerable<Vector2> region)
     => region.Sum(p => SquareGrid.FourDirAdjecentPoints(p)
                         .Count(p => !region.Contains(p)));
+
+IEnumerable<Vector2> GetPerimiterPoints(IEnumerable<Vector2> region)
+    => region.Where(p => SquareGrid.AdjacentPoints(p)   //Not 4 way because we want to include concave corners
+                        .Any(p => !region.Contains(p)));
+
+int GetCornerCount(IEnumerable<Vector2> region)
+{
+    //Only needs to check perimeter points
+    var perimiter = GetPerimiterPoints(region).ToList();
+    var corners = 0;
+
+    foreach (var p in perimiter)
+    {
+        //If the two adjacent points are not the same character, it is a convex corner
+        //If the two adjacent points are the same character and the diagonal is NOT the same character, it is a concave corner
+        //Otherwise it's not a corner
+        //We check all 4 directions  a single point can be multiple corners
+        var up = new Vector2(p.X, p.Y - 1);
+        var upRight = new Vector2(p.X + 1, p.Y - 1);
+        var right = new Vector2(p.X + 1, p.Y);
+        var downRight = new Vector2(p.X + 1, p.Y + 1);
+        var down = new Vector2(p.X, p.Y + 1);
+        var downLeft = new Vector2(p.X - 1, p.Y + 1);
+        var left = new Vector2(p.X - 1, p.Y);
+        var upLeft = new Vector2(p.X - 1, p.Y - 1);
+
+        //Up right convex
+        if (!region.Contains(up) && !region.Contains(right))
+            corners++;
+        //Up right concave
+        else if (region.Contains(up) && region.Contains(right) && !region.Contains(upRight))
+            corners++;
+
+        //Right down convex
+        if (!region.Contains(right) && !region.Contains(down))
+            corners++;
+        //Right down concave
+        else if (region.Contains(right) && region.Contains(down) && !region.Contains(downRight))
+            corners++;
+
+        //Down left convex
+        if (!region.Contains(down) && !region.Contains(left))
+            corners++;
+        //Down left concave
+        else if (region.Contains(down) && region.Contains(left) && !region.Contains(downLeft))
+            corners++;
+
+        //Left up convex
+        if (!region.Contains(left) && !region.Contains(up))
+            corners++;
+        //Left up concave
+        else if (region.Contains(left) && region.Contains(up) && !region.Contains(upLeft))
+            corners++;
+    }
+
+    return corners;
+}
+
+void PrintRegion(IEnumerable<Vector2> region)
+{
+    var minX = (int)region.Min(p => p.X);
+    var minY = (int)region.Min(p => p.Y);
+    var maxX = (int)region.Max(p => p.X);
+    var maxY = (int)region.Max(p => p.Y);
+    for (int y = minY; y <= maxY; y++)
+    {
+        for (int x = minX; x <= maxX; x++)
+        {
+            if (region.Contains(new Vector2(x, y)))
+                Console.Write(input[y][x]);
+            else
+                Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
