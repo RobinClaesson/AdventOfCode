d9dd2ebd85c2e5194233891ccc67d173654f9b33
Robin Claesson <57729281+RobinClaesson@users.noreply.github.com>
2025-12-01 19:38:29 +0100

    C# 2025 Day 1
    Honestly part 2 solution is so stupid but it works

---
+++ C#/2025/Day1/Program.cs
@@ -0,0 +1,33 @@
+﻿using AoC.IO;
+
+Input.TestMode = false;
+var input = Input.Rows
+    .Select(row => new Move(row[0] == 'R', int.Parse(row[1..])))
+    .ToList();
+
+Output.Answer(GetPassword(input));
+
+var expandedInput = new List<Move>();
+input.ForEach(move => expandedInput.AddRange(Enumerable.Repeat(move with { Steps = 1 }, move.Steps)));
+Output.Answer(GetPassword(expandedInput));
+
+int PositiveMod(int x, int mod) => (x % mod + mod) % mod;
+
+int GetPassword(List<Move> moves)
+{
+    var password = 0;
+    var dial = 50;
+
+    foreach (var move in moves)
+    {
+        dial += move.Right ? move.Steps : -move.Steps;
+
+        dial = PositiveMod(dial, 100);
+        if (dial == 0)
+            password++;
+    }
+
+    return password;
+}
+
+internal record Move(bool Right, int Steps);
