9e8947488cc2a8466851471811908156ca7e4067
Robin Claesson <57729281+RobinClaesson@users.noreply.github.com>
2025-12-02 20:00:58 +0100

    C# 2025 Day 2

---
+++ C#/2025/Day2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+using AoC.IO;
+
+//Capture some digits at the start of a line,
+//then the exact same digits repeat one additional times until the end of the line
+var part1InvalidRegex = new Regex(@"^(\d+)\1$", RegexOptions.Multiline);
+
+//Capture some digits at the start of a line,
+//then the exact same digits repeat one or more additional times until the end
+var part2InvalidRegex = new Regex(@"^(\d+)(?:\1)+$", RegexOptions.Multiline);
+
+Input.TestMode = false;
+var input = Input.Split(',')
+    .Select(rangeString => rangeString.Split('-', StringSplitOptions.RemoveEmptyEntries))
+    .Select(ids => new IdRange(long.Parse(ids[0]), long.Parse(ids[1]))).ToList();
+
+Output.Answer(input.SelectMany(range => FindInvalidIds(range, part1InvalidRegex)).Sum());
+Output.Answer(input.SelectMany(range => FindInvalidIds(range, part2InvalidRegex)).Sum());
+
+List<long> FindInvalidIds(IdRange range, Regex invalidIdRegex)
+{
+    var invalidIds = new List<long>();
+    for (var id = range.First; id <= range.Last; id++)
+    {
+        if (invalidIdRegex.IsMatch(id.ToString()))
+            invalidIds.Add(id);
+    }
+
+    return invalidIds;
+}
+
+internal record IdRange(long First, long Last);
