8464a4a193e74b1d228bbb9cf67c8a0b00da5ee0
Robin Claesson <57729281+RobinClaesson@users.noreply.github.com>
2025-12-03 21:05:33 +0100

    C# 2025 Day 3

---
+++ C#/2025/Day3/Program.cs
@@ -0,0 +1,22 @@
+﻿using AoC.IO;
+
+Input.TestMode = false;
+var input = Input.IntGrid;
+
+Output.Answer(input.Select(bank => GetMaxJoltages(bank, 2)).Sum());
+Output.Answer(input.Select(bank => GetMaxJoltages(bank, 12)).Sum());
+return;
+
+long GetMaxJoltages(List<int> bank, int batteryCount)
+{
+    if (batteryCount == 1)
+        return bank.Max();
+
+    if (bank.Count == batteryCount)
+        return bank.Aggregate(0L, (acc, battery) => 10 * acc + battery);
+    
+    var current = bank.Take(bank.Count - batteryCount + 1).Max();
+    var following = GetMaxJoltages(bank.Skip(bank.IndexOf(current) + 1).ToList(), batteryCount - 1);
+
+    return long.Parse($"{current}{following}");
+}
