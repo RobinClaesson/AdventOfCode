1e8c685957e580b6b26e9eba818e18550f5ae364
Robin Claesson <57729281+RobinClaesson@users.noreply.github.com>
2025-12-20 11:28:12 +0100

    Added interval restricted logging to Output

--- C#/AoC_DotNet6/IO/Output.cs
+++ C#/AoC_DotNet6/IO/Output.cs
@@ -14,6 +14,34 @@ namespace AoC.IO
                 TextCopy.ClipboardService.SetText($"{answer}");
         }
 
-        
+        /// <summary>
+        /// The minium time that has to pass between <see cref="Log"/> prints. <br />
+        /// Default: <c>1 second</c>
+        /// </summary>
+        public static TimeSpan MinLogInterval { get; set; } = TimeSpan.FromSeconds(1);
+
+        /// <summary>
+        /// UTC time for last <see cref="Log"/> print
+        /// </summary>
+        public static DateTime LastLogAtUtc { get; private set; } = DateTime.MinValue;
+
+        /// <summary>
+        /// Prints a log message with the given object if at least <see cref="MinLogInterval"/> time
+        /// has passed since last log print.
+        /// </summary>
+        /// <returns><c>true</c> if a log was printed, otherwise <c>false</c></returns>
+        /// <param name="toLog">Objects to log</param>
+        public static bool Log(params object[] toLog)
+        {
+            var now = DateTime.UtcNow;
+            var interval = now - LastLogAtUtc;
+
+            if (interval < MinLogInterval)
+                return false;
+
+            LastLogAtUtc = now;
+            Console.WriteLine($"[{LastLogAtUtc}] {string.Join(" ", toLog)}");
+            return true;
+        }
     }
 }
